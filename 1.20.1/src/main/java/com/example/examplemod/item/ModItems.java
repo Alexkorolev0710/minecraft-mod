fcd485c760990e9e34217166a69387d1c52223e4
BALARA <1fewl1@mail.ru>
2024-04-14 19:33:17 +0300

    Update ModItems.java

--- 1.20.1/src/main/java/com/example/examplemod/item/ModItems.java
+++ 1.20.1/src/main/java/com/example/examplemod/item/ModItems.java
@@ -33,8 +33,19 @@ public class ModItems {
             () -> new Item(new Item.Properties().food(ModFoods.CANDY_FROM_SANTA)));
     public static final RegistryObject<Item> PINE_CONE= ITEMS.register("pine_cone",
             () -> new FuelItem(new Item.Properties(),400));
-    public static final RegistryObject<Item> SAPPHIRE_AXE= ITEMS.register("sapphire_axe",
-            () -> new Item(new Item.Properties()));
+            
+    public static final RegistryObject<Item> SAPPHIRE_SWORD = ITEMS.register("sapphire_sword",
+            () -> new SwordItem(ModToolTiers.SAPPHIRE, 4, 2, new Item.Properties()));
+    public static final RegistryObject<Item> SAPPHIRE_PICKAXE = ITEMS.register("sapphire_pickaxe",
+            () -> new PickaxeItem(ModToolTiers.SAPPHIRE, 1, 1, new Item.Properties()));
+    public static final RegistryObject<Item> SAPPHIRE_AXE = ITEMS.register("sapphire_axe",
+            () -> new AxeItem(ModToolTiers.SAPPHIRE, 7, 1, new Item.Properties()));
+    public static final RegistryObject<Item> SAPPHIRE_SHOVEL = ITEMS.register("sapphire_shovel",
+            () -> new ShovelItem(ModToolTiers.SAPPHIRE, 0, 0, new Item.Properties()));
+    public static final RegistryObject<Item> SAPPHIRE_HOE = ITEMS.register("sapphire_hoe",
+            () -> new HoeItem(ModToolTiers.SAPPHIRE, 0, 0, new Item.Properties()));
+
+
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
